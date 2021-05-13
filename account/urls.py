60cb0982db350c8f9f793cad279486df9da8982c
biopaolo95 <paolobiondo@outlook.com>
2021-05-14 00:18:46 +0200

    First_setup

---
+++ account/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path,include
+from account import views
+
+app_name = 'account'
+
+urlpatterns = [
+   path('',views.Index.as_view() ,name='index'),
+]
--- adm/settings.py
+++ adm/settings.py
@@ -8,13 +8,23 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
+
+
+WHAT INSTALLED:
+pip install psycopg2-binary         -> database
+pip install --user argon2-cffi             -> criptation password
+
 """
 
+
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_DIR = BASE_DIR / 'static'
+MEDIA_DIR = BASE_DIR / 'media'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -37,6 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +67,7 @@ ROOT_URLCONF = 'adm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,13 +89,17 @@ WSGI_APPLICATION = 'adm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3' ,
     }
 }
 
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher'
+]
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -105,13 +122,20 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Rome'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
+
+if DEBUG == False:
+    CSRF_COOKIE_SECURE = True #to avoid transmitting the CSRF cookie over HTTP accidentally.
+    SESSION_COOKIE_SECURE = True #to avoid transmitting the session cookie over HTTP accidentally.
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_SSL_REDIRECT = True
 
 
 # Static files (CSS, JavaScript, Images)
@@ -123,3 +147,25 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# GENERAL SETTINGS
+SITE_ID = 1
+LOGIN_URL='login'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = (BASE_DIR / 'static',)
+
+if DEBUG == False:
+    STATIC_ROOT = BASE_DIR / 'static'
+
+# VARIABLES SETTINGS
+ACTIVATION_REGISTRATION = True
